ae578726c073d1f9f06788e591c264e16800ec97
Kevin Corso <email": null>
2016-01-06 15:48:55 -0600

    Add login and registration view and controller

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,17 @@
+get '/login' do
+  erb :'/users/login'
+end
+
+post 'login' do
+  @user = User.find_by(username: params[:login][:username])
+  if @user.authenticate(@user.username, @user.password)
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    erb :'/users/new'
+  end
+end
+
+post '/logout' do
+
+end
