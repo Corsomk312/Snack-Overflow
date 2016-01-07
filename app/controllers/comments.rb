c386f0bb06e7dc15c6447365178d4e90fb6f9b86
Kevin Corso <email": null>
2016-01-07 15:03:50 -0600

    Fix hard-coded question assignment for comments on Qs and As

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,32 +1,33 @@
 post '/questions/:question_id/comments' do
 	@comment = Comment.new(params[:comment])
 	if @comment.save
-		@question = Question.first 
-		redirect "/questions/#{@question.id}/answers"
+		@question_id = params[:comment][:commentable_id]
+		redirect "/questions/#{@question_id}/answers"
 	else
-		@error = "Sorry, server error" # Tell Pamela to display server error in the show answer view 
-		erb :'/questions/#{@question.id}/answers' 
+		@error = "Sorry, server error" # Tell Pamela to display server error in the show answer view
+		erb :'/questions/#{@question.id}/answers'
 	end
 end
 
 post '/answers/:answer_id/comments' do
-@comment = Comment.new(params[:comment])
+	@comment = Comment.new(params[:comment])
 	if @comment.save
-		@question = Question.first
-		redirect "/questions/#{@question.id}/answers"
+		@answer_id = params[:comment][:commentable_id]
+		question = Answer.find(@answer_id).question
+		redirect "/questions/#{question.id}/answers"
 	else
-		@error = "Sorry, server error" # Tell Pamela to display server error in the show answer view 
-		erb :'/questions/#{@question.id}/answers' 
+		@error = "Sorry, server error" # Tell Pamela to display server error in the show answer view
+		erb :'/questions/#{@question.id}/answers'
 	end
 end
 
 
 get '/comments' do
-	@question = Question.first 
-	erb :'/comments/new' 
+	@question = Question.first
+	erb :'/comments/new'
 end
 
 get '/commentss' do
-	@answer = Answer.first 
-	erb :'/comments/new_answer_comment' 
+	@answer = Answer.first
+	erb :'/comments/new_answer_comment'
 end
