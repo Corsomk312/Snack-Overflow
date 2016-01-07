382274ba14c575be4a7e8afdd02212a3df9664b1
Pamela Antonow <unstyleurbain@gmail.com>
2016-01-07 14:21:31 -0600

    Complete the view for answer

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+  $("#answer_form").on("click",function(event){
+    event.preventDefault();
+     var link = $(this).children()[0];
+     var route = $(link).attr('href')
+      $(this).hide()
+
+      var request = $.ajax({
+        method: "get",
+        url: route
+      });
+
+      request.done(function(responseData){
+        console.log(responseData)
+        $(".answer-container").append(responseData);
+      });
+
+
+
+  });
+
 
 });
