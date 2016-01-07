8280ba9fb14040220c83f178e6e7e243aa734a46
nowakoff <anowak@icstars.org>
2016-01-07 09:53:23 -0600

    Add a seed file

--- db/migrate/20160106133900_create_comments.rb
+++ db/migrate/20160106133900_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.references :user 
       t.integer  :commentable_id
       t.string   :commentable_type
       t.text     :content
--- db/migrate/20160106133904_create_votes.rb
+++ db/migrate/20160106133904_create_votes.rb
@@ -1,6 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
       create_table :votes do |t|
+      t.references :user 
       t.integer    :voteable_id
       t.string     :voteable_type
       t.string     :value
