83044245d2c27668378038c8be17eb152e42aebb
jelliotartz <jelliotartz@gmail.com>
2016-01-07 09:04:46 -0600

    edit question controller routes

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -5,15 +5,15 @@ get '/questions' do
 end
 
 # get the form to create a question
-get "/users/:user_id/question/new" do
+get "/question/new" do
 	erb :"questions/new"
 end
 
 # create a new question from the form
-post "/users/:user_id/questions" do
-  current_user.questions.create(params[:question])
+post "/questions" do
+	if current_user
+  Question.create(params[:question])
   redirect "/users/#{current_user.id}"
+	end
 end
-
-
 
