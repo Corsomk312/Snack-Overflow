f9d4b7fc4c5c808600eb3e4f537fd4c682c466d7
Kevin Corso <email": null>
2016-01-07 17:15:55 -0600

    add upvote and downvote arrows to questions/show page

---
+++ app/controllers/votes.rb
@@ -0,0 +1,7 @@
+post '/questions/:question_id/vote' do
+
+end
+
+post '/answers/:answer_id/vote' do
+
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :votes, as: :voteable
   belongs_to :user
+
+  def points
+    self.votes.count
+  end
 end
