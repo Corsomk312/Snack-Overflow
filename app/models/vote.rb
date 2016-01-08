909112a0557bd536218787749732dd1d057b82af
Kevin Corso <email": null>
2016-01-07 18:37:59 -0600

    change method name to get net votes for questions

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
   belongs_to :user
 
-  def net_votes(question_id)
+  def net_votes_question(question_id)
     question = Question.find(question_id)
     up = question.votes.where(value: "up").count
     down = question.votes.where(value: "down").count
