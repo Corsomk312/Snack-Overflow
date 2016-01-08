9def3c06e5ca15819ba91a373fac2e108d3faa3d
Kevin Corso <email": null>
2016-01-07 18:36:04 -0600

    add upvote and downvote capabilites for main questions show page?

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,7 +1,36 @@
-post '/questions/:question_id/vote' do
+get '/questions/:question_id/vote' do
+  question = Question.find(params[:question_id])
+  vote = question.votes.build(value: "up")
 
+  if vote.save
+    current_user.votes << vote
+    points = vote.net_votes(params[:question_id])
+    if request.xhr?
+      content_type :json
+      { points: points}.to_json
+    else
+      error = "you done fucked up"
+    end
+  else
+    redirect '/questions/:question_id/answers'
+  end
 end
 
-post '/answers/:answer_id/vote' do
+get '/questions/:question_id/downvote' do
+  question = Question.find(params[:question_id])
+  vote = question.votes.build(value: "down")
 
+  if vote.save
+    current_user.votes << vote
+    points = vote.net_votes(params[:question_id])
+
+    if request.xhr?
+      content_type :json
+      { points: points}.to_json
+    else
+      error = "you done fucked up"
+    end
+  else
+    redirect '/questions/:question_id/answers'
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -53,8 +53,55 @@ $(document).ready(function () {
       $('#submit_answer_comment')[0].reset();
     });
   });
-    
+
 });
+
+$(document).ready(function() {
+  // This is called after the document has loaded in its entirety
+  // This guarantees that any elements we bind to will exist on the page
+  // when we try to bind to them
+
+
+  $(".vote-button").click(function(event){
+    event.preventDefault();
+    var path = $(this).attr('href');
+    var id = $(this).parent().attr('id');
+
+
+    var request = $.ajax({
+    method: "GET",
+    url: path,
+    });
+
+    request.done(function(responseData){
+      $('#' + id +' .vote-button').css('color', 'black');
+      var points = $('#' + id +' .points');
+      points.html(responseData.points);
+    })
+
+    request.fail(function(response){
+      alert('Your book is overdue!' );
+    })
+  })
+
+  $(".down-vote-button").click(function(event){
+    event.preventDefault();
+    var path = $(this).attr('href');
+    var id = $(this).parent().attr('id');
+
+
+    var request = $.ajax({
+    method: "GET",
+    url: path,
+    });
+
+    request.done(function(responseData){
+      $('#' + id +' .vote-button').css('color', 'red');
+      var points = $('#' + id +' .points');
+      points.html(responseData.points);
+    })
+  })
+})
 
 
 
